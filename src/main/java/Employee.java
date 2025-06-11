8db5922f687dcfc74602106e16c00e1d75c09cdb
krosengr4 <rosenkev4@gmail.com>
2025-06-11 15:00:40 -0500

    Created employee class that implements Printable interface

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,18 @@
-public class Employee {
+public class Employee implements Printable{
+
+    String firstName;
+    String lastName;
+    String title;
+
+    public Employee(String firstName, String lastName, String title) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+    }
+
+    public void print() {
+        System.out.println("First Name: " + this.firstName);
+        System.out.println("Last Name: " + this.lastName);
+        System.out.println("Title: " + this.title);
+    }
 }
