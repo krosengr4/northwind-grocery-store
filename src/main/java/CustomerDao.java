75ecab75dc293240165c827a7ee223b562f33569
krosengr4 <rosenkev4@gmail.com>
2025-06-11 18:59:01 -0500

    Created method to get a single customer

--- src/main/java/CustomerDao.java
+++ src/main/java/CustomerDao.java
@@ -38,4 +38,32 @@ public class CustomerDao {
 
         return customersList;
     }
+
+    public Customer getCustomer(String query, String userInput) {
+        Customer customer = null;
+
+        try (Connection conn = dataSource.getConnection()) {
+
+            PreparedStatement prepStatement = conn.prepareStatement(query);
+            prepStatement.setString(1, userInput);
+
+            ResultSet results = prepStatement.executeQuery();
+
+            while (results.next()) {
+                String customerName = results.getString("ContactName");
+                String companyName = results.getString("CompanyName");
+                String city = results.getString("City");
+                String country = results.getString("Country");
+                String phoneNumber = results.getString("Phone");
+
+                customer = new Customer(customerName, companyName, city, country, phoneNumber);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return customer;
+    }
+
 }
