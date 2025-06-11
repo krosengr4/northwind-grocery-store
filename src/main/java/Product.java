924852d8709a743ec18d947c27129e17495d9571
krosengr4 <rosenkev4@gmail.com>
2025-06-11 15:01:51 -0500

    Created Product class that implements Printable interface

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,22 @@
-public class Product {
+public class Product implements Printable {
+
+    int productID;
+    String name;
+    double unitPrice;
+    int unitsInStock;
+
+    public Product (int productID, String name, double unitPrice, int unitsInStock) {
+        this.productID = productID;
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.unitsInStock = unitsInStock;
+    }
+
+    public void print() {
+        System.out.println("Product ID: " + this.productID);
+        System.out.println("Product Name: " + this.name);
+        System.out.printf("Unit Price: $%.2f\n", this.unitPrice);
+        System.out.println("Units In Stock: " + this.unitsInStock);
+    }
+
 }
