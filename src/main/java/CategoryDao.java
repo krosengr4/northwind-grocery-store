be58bcb9ddc5adc8aa760d9b5dd5625af52742d1
krosengr4 <rosenkev4@gmail.com>
2025-06-11 15:10:13 -0500

    Created CategoryDao class to get data from the data base and return it in a list, created method to get all categories

--- src/main/java/CategoryDao.java
+++ src/main/java/CategoryDao.java
@@ -1,2 +1,39 @@
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+
 public class CategoryDao {
+
+    private final DataSource dataSource;
+
+    public CategoryDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public ArrayList<Printable> getAllCategories() {
+
+        ArrayList<Printable> categoriesList = new ArrayList<>();
+
+        try (Connection conn = dataSource.getConnection()) {
+
+            String query = "SELECT * FROM categories ORDER BY CategoryID;";
+            PreparedStatement prepStatement = conn.prepareStatement(query);
+
+            ResultSet results = prepStatement.executeQuery();
+
+            while (results.next()) {
+                int catID = Integer.parseInt(results.getString("CategoryID"));
+                String catName = results.getString("CategoryName");
+                String catDescription = results.getString("Description");
+
+                Category newCategory = new Category(catID, catName, catDescription);
+                categoriesList.add(newCategory);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return categoriesList;
+    }
 }
