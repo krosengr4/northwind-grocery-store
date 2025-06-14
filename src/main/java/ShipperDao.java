a35a5aa41e40627fdd7c6b3ee7f1aa6a1732a098
krosengr4 <rosenkev4@gmail.com>
2025-06-14 04:38:56 -0500

    Created method to get a single shipper

--- src/main/java/ShipperDao.java
+++ src/main/java/ShipperDao.java
@@ -35,4 +35,27 @@ public class ShipperDao {
         return shippersList;
     }
 
+    public Shipper getShipper(String query, String userInput) {
+        Shipper shipper = null;
+
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, userInput);
+
+            ResultSet results = statement.executeQuery();
+            while (results.next()) {
+                int shipperID = Integer.parseInt(results.getString("ShipperID"));
+                String companyName = results.getString("CompanyName");
+                String phoneNumber = results.getString("Phone");
+
+                shipper = new Shipper(shipperID, companyName, phoneNumber);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return shipper;
+    }
+
 }
