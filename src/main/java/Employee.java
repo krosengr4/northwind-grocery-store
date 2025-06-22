5a84ba525ef214dd7de267736196fc90048def9b
krosengr4 <rosenkev4@gmail.com>
2025-06-22 00:33:07 -0500

    Added more fields to Employee class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -8,17 +8,21 @@ public class Employee implements NorthwindData {
     String title;
 //	Date birthDate;
 //	Date hireDate;
-//	String address;
-//	String city;
-//	String Country;
-//	String postalCode;
+	String address;
+	String city;
+	String country;
+	String postalCode;
    String notes;
 
-    public Employee(int employeeID, String firstName, String lastName, String title, String notes) {
+    public Employee(int employeeID, String firstName, String lastName, String title, String address, String city, String country, String postalCode, String notes) {
 	   this.employeeID = employeeID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.title = title;
+		this.address = address;
+		this.city = city;
+		this.country = country;
+		this.postalCode = postalCode;
 		this.notes = notes;
     }
 
@@ -28,5 +32,7 @@ public class Employee implements NorthwindData {
         System.out.println("First Name: " + this.firstName);
         System.out.println("Last Name: " + this.lastName);
         System.out.println("Title: " + this.title);
+	   System.out.println("City: " + this.city);
+	   System.out.println("Country: " + this.country);
     }
 }
