e4941dfd0d55e32d40a17a79c759667787712776
krosengr4 <rosenkev4@gmail.com>
2025-06-21 22:19:51 -0500

    Added mapRow method to ShipperDao and refactored code

--- src/main/java/ShipperDao.java
+++ src/main/java/ShipperDao.java
@@ -4,139 +4,133 @@ import java.util.ArrayList;
 
 public class ShipperDao {
 
-    private final DataSource dataSource;
+   private final DataSource dataSource;
 
-    public ShipperDao(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+   public ShipperDao(DataSource dataSource) {
+	  this.dataSource = dataSource;
+   }
 
-    public ArrayList<NorthwindData> getAllShippers() {
-        ArrayList<NorthwindData> shippersList = new ArrayList<>();
+   public ArrayList<NorthwindData> getAllShippers() {
+	  ArrayList<NorthwindData> shippersList = new ArrayList<>();
+	  String query = "SELECT * FROM shippers";
 
-        try (Connection conn = dataSource.getConnection()) {
+	  try(Connection conn = dataSource.getConnection()) {
 
-            String query = "SELECT * FROM shippers";
-            PreparedStatement statement = conn.prepareStatement(query);
+		 PreparedStatement statement = conn.prepareStatement(query);
 
-            ResultSet results = statement.executeQuery();
-            while (results.next()) {
-                int shipperID = results.getInt("ShipperID");
-                String companyName = results.getString("CompanyName");
-                String phoneNumber = results.getString("Phone");
+		 ResultSet results = statement.executeQuery();
+		 while(results.next()) {
+			Shipper newShipper = mapRow(results);
+			shippersList.add(newShipper);
+		 }
 
-                Shipper newShipper = new Shipper(shipperID, companyName, phoneNumber);
-                shippersList.add(newShipper);
-            }
+	  } catch(SQLException e) {
+		 throw new RuntimeException(e);
+	  }
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+	  return shippersList;
+   }
 
-        return shippersList;
-    }
+   public Shipper getShipper(String query, String userInput) {
 
-    public Shipper getShipper(String query, String userInput) {
-        Shipper shipper = null;
+	  try(Connection conn = dataSource.getConnection()) {
+		 PreparedStatement statement = conn.prepareStatement(query);
+		 statement.setString(1, "%" + userInput + "%");
 
-        try (Connection conn = dataSource.getConnection()) {
-            PreparedStatement statement = conn.prepareStatement(query);
-            statement.setString(1, "%" + userInput + "%");
+		 ResultSet results = statement.executeQuery();
+		 if(results.next()) {
+			return mapRow(results);
+		 }
 
-            ResultSet results = statement.executeQuery();
-            while (results.next()) {
-                int shipperID = results.getInt("ShipperID");
-                String companyName = results.getString("CompanyName");
-                String phoneNumber = results.getString("Phone");
+	  } catch(SQLException e) {
+		 throw new RuntimeException(e);
+	  }
 
-                shipper = new Shipper(shipperID, companyName, phoneNumber);
-            }
+	  return null;
+   }
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+   public Shipper getShipperById(int shipperId) {
+	  String query = "	SELECT * FROM shippers WHERE ShipperID = ?;";
 
-        return shipper;
-    }
+	  try(Connection conn = dataSource.getConnection()) {
+		 PreparedStatement statement = conn.prepareStatement(query);
+		 statement.setInt(1, shipperId);
 
-	public Shipper getShipperById(int shipperId) {
-	   Shipper shipper = null;
+		 ResultSet results = statement.executeQuery();
 
-	   String query = "	SELECT * FROM shippers WHERE ShipperID = ?;";
-	   try (Connection conn = dataSource.getConnection()) {
-		  PreparedStatement statement = conn.prepareStatement(query);
-		  statement.setInt(1, shipperId);
+		 if(results.next()) {
+			return mapRow(results);
+		 }
+	  } catch(SQLException e) {
+		 throw new RuntimeException(e);
+	  }
+	  return null;
+   }
 
-		  ResultSet results = statement.executeQuery();
-		  while(results.next()) {
-			 int shipperID = results.getInt("ShipperID");
-			 String categoryName = results.getString("CompanyName");
-			 String phoneNumber = results.getString("Phone");
+   public void addShipper(Shipper shipper) {
+	  String companyName = shipper.companyName;
+	  String phoneNumber = shipper.phoneNumber;
 
-			 shipper = new Shipper(shipperID, categoryName, phoneNumber);
-		  }
+	  String query = "INSERT INTO shippers (CompanyName, Phone) " +
+							 "VALUES (?, ?);";
+	  try(Connection conn = dataSource.getConnection()) {
+		 PreparedStatement statement = conn.prepareStatement(query);
+		 statement.setString(1, companyName);
+		 statement.setString(2, phoneNumber);
 
-	   } catch (SQLException e) {
-		  throw new RuntimeException(e);
-	   }
-	   return shipper;
-	}
+		 int rows = statement.executeUpdate();
+		 if(rows != 0) {
+			System.out.println("Success! Shipper was added!!!");
+		 } else {
+			System.err.println("ERROR! Could not add shipper!!!");
+		 }
 
-	public void addShipper(Shipper shipper) {
-	   String companyName = shipper.companyName;
-	   String phoneNumber = shipper.phoneNumber;
+	  } catch(SQLException e) {
+		 throw new RuntimeException(e);
+	  }
+   }
 
-	   String query = "INSERT INTO shippers (CompanyName, Phone) " +
-							  "VALUES (?, ?);";
-	   try (Connection conn = dataSource.getConnection()) {
-		  PreparedStatement statement = conn.prepareStatement(query);
-		  statement.setString(1, companyName);
-		  statement.setString(2, phoneNumber);
+   public void updateShipper(String query, int shipperId, String newValue) {
+	  try(Connection conn = dataSource.getConnection()) {
+		 PreparedStatement statement = conn.prepareStatement(query);
+		 statement.setString(1, newValue);
+		 statement.setInt(2, shipperId);
 
-		  int rows = statement.executeUpdate();
-		  if(rows != 0){
-			 System.out.println("Success! Shipper was added!!!");
-		  } else {
-			 System.err.println("ERROR! Could not add shipper!!!");
-		  }
+		 int rows = statement.executeUpdate();
+		 if(rows != 0) {
+			System.out.println("Success! Shipper information was updated!");
+		 } else {
+			System.err.println("Error! Could not update shipper information!!!");
+		 }
+	  } catch(SQLException e) {
+		 throw new RuntimeException(e);
+	  }
+   }
 
-	   } catch (SQLException e) {
-		  throw new RuntimeException(e);
-	   }
-	}
+   public void deleteShipper(int shipperId) {
+	  String query = "DELETE FROM shippers WHERE ShipperID = ?;";
 
-	public void updateShipper(String query, int shipperId, String newValue) {
-	   try(Connection conn = dataSource.getConnection()) {
-		  PreparedStatement statement = conn.prepareStatement(query);
-		  statement.setString(1, newValue);
-		  statement.setInt(2, shipperId);
+	  try(Connection conn = dataSource.getConnection()) {
+		 PreparedStatement statement = conn.prepareStatement(query);
+		 statement.setInt(1, shipperId);
 
-		  int rows = statement.executeUpdate();
-		  if(rows != 0) {
-			 System.out.println("Success! Shipper information was updated!");
-		  } else {
-			 System.err.println("Error! Could not update shipper information!!!");
-		  }
-	   } catch (SQLException e) {
-		  throw new RuntimeException(e);
-	   }
-	}
+		 int rows = statement.executeUpdate();
+		 if(rows != 0) {
+			System.out.println("Success! Shipper was deleted!");
+		 } else {
+			System.err.println("ERROR! Could not delete Shipper!");
+		 }
+	  } catch(SQLException e) {
+		 throw new RuntimeException(e);
+	  }
+   }
 
-	public void deleteShipper(int shipperId) {
-	   String query = "DELETE FROM shippers WHERE ShipperID = ?;";
+   private Shipper mapRow(ResultSet results) throws SQLException {
+	  int shipperID = results.getInt("ShipperID");
+	  String companyName = results.getString("CompanyName");
+	  String phoneNumber = results.getString("Phone");
 
-	   try(Connection conn = dataSource.getConnection()) {
-		  PreparedStatement statement = conn.prepareStatement(query);
-		  statement.setInt(1, shipperId);
-
-		  int rows = statement.executeUpdate();
-		  if(rows != 0 ) {
-			 System.out.println("Success! Shipper was deleted!");
-		  } else {
-			 System.err.println("ERROR! Could not delete Shipper!");
-		  }
-	   } catch (SQLException e) {
-		  throw new RuntimeException(e);
-	   }
-	}
+	  return new Shipper(shipperID, companyName, phoneNumber);
+   }
 
 }
