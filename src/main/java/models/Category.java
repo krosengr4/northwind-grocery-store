7c8449b4dd84a3afcd3128759b773942f006dcdb
Kevin Rosengren <rosenkev4@gmail.com>
2025-11-11 01:36:20 -0600

    Added getters and setters for category class

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -2,20 +2,46 @@ package models;
 
 public class Category implements NorthwindData {
 
-    public int categoryID;
-    public String name;
-    public String description;
+	public int categoryID;
+	public String name;
+	public String description;
 
-    public Category (int categoryID, String name, String description) {
-        this.categoryID = categoryID;
-        this.name = name;
-        this.description = description;
-    }
+	public Category(int categoryID, String name, String description) {
+		this.categoryID = categoryID;
+		this.name = name;
+		this.description = description;
+	}
 
-    public void print() {
-        System.out.println("-----CATEGORY-----");
-        System.out.println("Category ID: " + this.categoryID);
-        System.out.println("Category Name: " + this.name);
-        System.out.println("Category Description: " + this.description);
-    }
+	//	region Getters and Setters
+	public int getCategoryID() {
+		return categoryID;
+	}
+
+	public void setCategoryID(int categoryID) {
+		this.categoryID = categoryID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+//	endregion
+
+	public void print() {
+		System.out.println("-----CATEGORY-----");
+		System.out.println("Category ID: " + this.categoryID);
+		System.out.println("Category Name: " + this.name);
+		System.out.println("Category Description: " + this.description);
+	}
 }
